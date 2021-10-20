30c77bd195c22427e56df72f7e58a490ceee5abd
Onuralp <xonuraralpx@gmail.com>
2021-10-20 11:41:33 +0300

    Initial Commit

---
+++ Homework Tableview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework Tableview
+//
+//  Created by Ceren Çapar on 20.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
